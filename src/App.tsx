44cf6422e97e9cf0cf31d3923440e5f429dd661e
MinJuPark52 <als4013@naver.com>
2025-02-02 21:20:44 +0900

    Nav 고정

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import React, { useState, useEffect } from "react";
-import Nav from "./components/Nav.tsx";
 import Footer from "./components/Footer.tsx";
 import Products from "./components/Products.tsx";
 import Slide from "./components/Slide.tsx";
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      setIsDarkMode(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [isDarkMode]);
-
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
-      <Nav isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
       <div className="mt-20">
         <Slide />
       </div>
