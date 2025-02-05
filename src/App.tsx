eb9c1dba77ae8514826594b07ed18592b7b2f9ca
MinJuPark52 <als4013@naver.com>
2025-02-05 14:38:36 +0900

    fix:레이아웃 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Slide from "./components/Slide.tsx";
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
-      <div className="mt-8">
+      <div className="mt-20">
         <Slide />
       </div>
       <div className="mt-2">
--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,16 +1,10 @@
 const CartPage = () => {
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-      <div className="mx-auto max-w-3xl">
-        <header className="text-center">
-          <h1 className="text-xl font-bold text-gray-900 sm:text-3xl">
-            Your Cart
-          </h1>
-        </header>
-
+    <div className="w-[1020px]  px-4 py-8">
+      <div className="mx-auto ">
         <div className="mt-8">
           <ul className="space-y-4">
-            <li className="flex items-center gap-4">
+            <li className="flex gap-4">
               <img
                 src="https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=830&q=80"
                 alt=""
