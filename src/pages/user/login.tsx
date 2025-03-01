d517068115e9d9eb1633198d52ae2041120045a9
MinJuPark52 <als4013@naver.com>
2025-03-01 23:59:00 +0900

    build: 빌드 수정

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import LoginStore from "../../stores/loginStore";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { firebaseConfig } from "firebaseConfig";
+import { firebaseConfig } from "../../firebase/firebaseConfig";
 import { initializeApp } from "firebase/app";
 
 const LoginPage: React.FC = () => {
