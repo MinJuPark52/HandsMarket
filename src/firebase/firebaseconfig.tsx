a6fc70745d4c2b96acac3f52fb65df2e57190f8d
MinJuPark52 <als4013@naver.com>
2025-03-02 16:10:52 +0900

    build: firebase

--- src/firebase/firebaseconfig.tsx
+++ src/firebase/firebaseconfig.tsx
@@ -1,17 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const firebaseconfig = {
+export const firebaseconfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  messagesSenderId: process.env.REACT_APP_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
-const app = initializeApp(firebaseconfig);
-const auth = getAuth(app);
-
-export { auth };
