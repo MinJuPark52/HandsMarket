2cde0574932e7eb910bea889efc5cc2a0dd8f0fb
MinJuPark52 <als4013@naver.com>
2025-03-02 17:11:57 +0900

    build: firebase

--- src/pages/user/login.tsx
+++ src/pages/user/login.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import LoginStore from "../../stores/loginStore";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { firebaseconfig } from "../../../firebaseconfig";
+import { firebaseConfig } from "../../firebase/firebaseconfig";
 import { initializeApp } from "firebase/app";
 
 const LoginPage: React.FC = () => {
   const { id, password, error, setId, setPassword, setError } = LoginStore();
   const navigate = useNavigate();
-  const app = initializeApp(firebaseconfig);
+  const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
 
   const loginSubmit = async (e: any) => {
