0b261c441e58ef830c3b838b72de0e9fb8566e8a
MinJuPark52 <als4013@naver.com>
2025-03-02 14:27:14 +0900

    fix: firebase

--- src/firebaseconfig.tsx
+++ src/firebaseconfig.tsx
@@ -2,7 +2,7 @@ export const firebaseconfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "eshop-e614e.firebaseapp.com",
   projectId: "eshop-e614e",
-  storageBucket: "eshop-e614e.firebasestorage.app",
+  storageBucket: "eshop-e614e.appspot.com",
   messagingSenderId: "272954111023",
   appId: "1:272954111023:web:351cec4312189ed68e82da",
   measurementId: "G-4KXN3S2QKC",
