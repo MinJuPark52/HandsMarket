8de3b17912de2d468c9be1dc379e9d2f3d811eac
MinJuPark52 <als4013@naver.com>
2025-03-02 17:12:40 +0900

    build: firebase

---
+++ src/firebase/firebaseConfig.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagesSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
